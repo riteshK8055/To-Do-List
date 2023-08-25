889c4bc1293fbe7a830508639d70e3d7ce838aa5
Ritesh Kumar <riteshk8055@gmail.com>
2023-08-25 16:58:38 +0530

    javascript code complete with all the required functionality added

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ const inputBox = document.getElementById('inputBox');
 const addBtn = document.getElementById('addBtn');
 const todoList = document.getElementById('todoList');
 
+let editTodo = null;
+
 const addTodo = ()=>{
 
    const inputText =inputBox.value.trim();
    if(inputText.length <=0){
 
     alert("you must write something in your todo");
+    return false;
+   }
+
+   if(addBtn.value === "Edit"){
+
+      editTodo.target.previousElementSibling.innerHTML = inputText;
+
+      addBtn.value = "Add";
+      inputBox.value ="";
    }
 
    else{
@@ -33,4 +44,23 @@ const addTodo = ()=>{
    inputBox.value = '';
    }
 }
+
+const updateTodo = (e)=>{
+
+   //console.log(e.target.innerHTML);
+
+   if(e.target.innerHTML === "Remove"){
+
+     todoList.removeChild(e.target.parentElement);
+   }
+
+   if(e.target.innerHTML ==="Edit"){
+
+      inputBox.value = e.target.previousElementSibling.innerHTML;
+      inputBox.focus();
+      addBtn.value = "Edit";
+      editTodo = e;
+   }
+}
 addBtn.addEventListener('click', addTodo);
+todoList.addEventListener('click',updateTodo);
